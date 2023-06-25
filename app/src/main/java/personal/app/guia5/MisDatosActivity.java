3928dd23db2d41c696a5c8fb5ec3c671b4333d49
EliseoCr <90576993+EliseoCr@users.noreply.github.com>
2023-06-24 22:50:42 -0600

    Proyecto Guia5 V1-prueba

---
+++ app/src/main/java/personal/app/guia5/MisDatosActivity.java
@@ -0,0 +1,14 @@
+package personal.app.guia5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MisDatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_datos);
+    }
+}
---
+++ app/src/main/java/personal/app/guia5/models/ConversationModel.java
@@ -0,0 +1,58 @@
+package personal.app.guia5.models;
+
+import java.io.Serializable;
+
+public class ConversationModel implements Serializable {
+
+    private int imgConversation;
+    private String names, message, time;
+
+    public ConversationModel(int imgConversation, String names, String message, String time) {
+        this.imgConversation = imgConversation;
+        this.names = names;
+        this.message = message;
+        this.time = time;
+    }
+
+    public int getImgConversation() {
+        return imgConversation;
+    }
+
+    public void setImgConversation(int imgConversation) {
+        this.imgConversation = imgConversation;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "ConversationModel{" +
+                "imgConversation=" + imgConversation +
+                ", names='" + names + '\'' +
+                ", message='" + message + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
